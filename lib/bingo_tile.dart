ab3f323b9d5694a94d85146311aeffed7e267de9
LucasS01 <Lucasschlierf@gmail.com>
2022-03-23 07:56:52 +0100

    removed unused method

--- lib/bingo_tile.dart
+++ lib/bingo_tile.dart
@@ -1,23 +1,5 @@
-import 'package:flutter/material.dart';
-
 class BingoTile {
   bool isMarkedOff = false;
   String text;
   BingoTile({required this.text});
-  Container getDisplayTile() {
-    return Container(
-      child: Center(
-        child: Column(
-          children: [
-            Text(text),
-            Checkbox(
-                value: isMarkedOff,
-                onChanged: (newValue) {
-                  isMarkedOff = newValue!;
-                })
-          ],
-        ),
-      ),
-    );
-  }
 }
